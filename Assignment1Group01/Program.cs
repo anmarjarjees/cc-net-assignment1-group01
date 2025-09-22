17133ef0d2653fa858feffe52322cb3a8d38e2d7
Anmar Jarjees <anmarjarjees@users.noreply.github.com>
2025-09-22 20:49:25 +0000

    Final Update

--- Assignment1Group01/Program.cs
+++ Assignment1Group01/Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+Console.WriteLine("I hope that whatever I say, makes sense to you :-)");
 // Continue your Assignment code...
